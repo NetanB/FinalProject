d33601055fa580332934dce907972a0673c73863
NetanB <200479857@student.georgianc.on.ca>
2026-04-15 21:22:08 -0400

    health ui and pickup

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -14,4 +14,6 @@ public class CameraController : Singleton<CameraController>
         cinemachineVirtualCamera = FindAnyObjectByType<CinemachineCamera>();
         cinemachineVirtualCamera.Follow = PlayerController.Instance.transform;
     }
+
+    
 }
